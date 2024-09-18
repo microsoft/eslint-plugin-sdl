5d84a956633518ec61b6ef2d27f4b8fe58e4d0dd
Jesse Parsons <jeparson@microsoft.com>
2024-09-18 14:41:30 -0700

    File exists?

--- tests/lib/test-utils.js
+++ tests/lib/test-utils.js
@@ -11,6 +11,12 @@ const path = require("path");
 
 console.log("dir" + __dirname);
 console.log("tsconfig" + path.join(__dirname, '../fixtures/tsconfig.json'));
+// check if file exists
+const fs = require('fs');
+const tsconfigPath = path.join(__dirname, '../fixtures/tsconfig.json');
+if (fs.existsSync(tsconfigPath)) {
+    console.log("tsconfig exists");
+}
 
 module.exports = {
     tsParserOptions: {
