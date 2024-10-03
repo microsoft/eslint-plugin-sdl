fc6f87eb2c00adc51b0c55fb51647053f4ef1245
Martin Kačmar <martin@kacmar.sk>
2024-10-03 10:57:55 +0200

    Correct react/iframe-missing-sandbox rule name

--- config/react.js
+++ config/react.js
@@ -30,7 +30,7 @@ module.exports = (pluginSdl) => {
             warnOnSpreadAttributes: true
           }
         ],
-        "react/react-iframe-missing-sandbox": "error"
+        "react/iframe-missing-sandbox": "error"
       }
     },
     {
